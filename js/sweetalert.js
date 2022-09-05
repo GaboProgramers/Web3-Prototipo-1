0f053a91c69bd30551d2d533f7af53d54fe89872
GaboProgramers <gutierrezyosguti@gmail.com>
2022-09-04 22:13:10 -0500

    agregado seccion para faucet (version 1.1) Version Global  V.43

---
+++ js/sweetalert.js
@@ -0,0 +1,20 @@
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+      })
+      
+      Toast.fire({
+        icon: 'success',
+        title: 'Roll Seleccionado'
+      })
+})
